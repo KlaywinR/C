a65b2be8d1eaf94179fdd0e2545fccf65195033f
Klaywin Ryan <klaywindis@gmail.com>
2025-06-02 17:53:26 -0300

    decrescente em c

---
+++ aulas_prog/decrescente.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main(void){
+     
+
+    
+
+     for (int i = 100; i >= 40; i--)
+          {
+             
+            // for (int i = 40; i <=  100; i++)
+        if( i % 7 == 0){
+             printf("%in", i);
+        }
+       
+     }
+     
+
+
+
+
+
+
+
+    return 0;
+}
