6c8d0637cddb0c1d06cacbc362aef5b26b3fd52b
Klaywin Ryan <klaywindis@gmail.com>
2025-06-09 21:19:47 -0300

    entrada e saida de dados

---
+++ curso_de_c_ytb/entrada_e_saida_de_dados.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(void)
+{
+
+  int idade;
+  printf(" DIGITE A SUA IDADE AQUI : "); // ENTRADA DE DADOS
+  scanf("%i", &idade);                   //  SAIDA DE DADOS.
+  printf("A SUA IDADE EH %i", idade);
+
+  return 0;
+}
