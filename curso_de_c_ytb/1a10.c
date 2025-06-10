9edfb2c2ab1d391509160a08c6d37e2057e6da45
Klaywin Ryan <klaywindis@gmail.com>
2025-06-09 21:18:14 -0300

    trabalhando de 1 a 10

---
+++ curso_de_c_ytb/1a10.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(void){
+    int numero;
+   
+    for (numero  = 1; numero <= 10 ; numero++)
+    {
+        printf("%i", &numero);
+    }
+    
+    
+
+
+
+
+
+    return 0;
+}
