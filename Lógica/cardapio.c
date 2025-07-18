c2076b514ac541142e78c5a86d157d8819e16437
Klaywin Ryan <klaywindias@gmail.com>
2025-07-18 15:29:11 -0300

    Create cardapio.c

---
+++ Lógica/cardapio.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main (){
+      
+    int numero_cardapio;
+    printf("DIGITE O NUMERO DO CARDAPIO QUE VC QUER!");
+
+     scanf("%i", &numero_cardapio);
+   
+
+       printf("VOCE ESCOLHEU O CARDAPIO DE NUMERO: %i\n", numero_cardapio);
+
+
+
+
+
+    return 0;
+}
