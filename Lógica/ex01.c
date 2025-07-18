4f9cc353026909e4f11f1518de52755e7ab4c90d
Klaywin Ryan <klaywindias@gmail.com>
2025-07-18 15:29:18 -0300

    Create ex01.c

---
+++ Lógica/ex01.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+  float numero_telefone;
+  printf("DIGITE O NUMERO DE TELEFONE QUE VC DESEJA GUARDAR ");
+  scanf("%f", &numero_telefone);
+
+  printf("O NUMERO DO TELEFONE E: %f", numero_telefone);
+
+    return 0;
+}
