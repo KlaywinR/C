d201e12c8d313587462489224cd15f5e7e9bfb95
Klaywin Ryan <klaywindias@gmail.com>
2025-07-25 11:35:06 -0300

    Create q10.c

---
+++ LISTA_03_c/q10.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(void)
+{
+  /**
+   * Implemente um programa em linguagem C que exiba em tela todos os números primos existentes
+ entre 1 e 120. Lembre-se que um número primo é um número natural maior que 1 que só é divisível
+ por 1 e por ele mesmo.
+   */
+  int i, j, primo;
+  for (i = 2; i <= 120; i++)
+  {
+    primo == 2;
+  }
+  for (j = 2; j < primo; j++)
+  {
+  }
+
+  return 0;
+}
