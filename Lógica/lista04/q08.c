a1a5def9876e1b9e5523207d1676759eb4c71825
Klaywin Ryan <klaywindias@gmail.com>
2025-07-25 11:35:09 -0300

    Create q08.c

---
+++ Lógica/lista04/q08.c
@@ -0,0 +1,34 @@
+/**
+ * Desenvolva um programa que apresente o seguinte menu: 
+1- SOMA 
+2- SUBTRAÇÃO 
+3- MULTIPLICAÇÃO 
+4- DIVISÃO 
+DIGITE SUA ESCOLHA
+E permita o usuário digitar um número escolhendo um dos itens listados no 
+menu. Para cada item receba dois números e realize a operação indicada, ao final 
+printar na tela o resultado. Exemplo: 
+Se o usuário digitar 1, deve receber dois números do usuário e depois printar a 
+SOMA deles. 
+ */
+#include <stdio.h>
+
+
+int main(){
+  float num1, num2, resultado;
+  int opcao;
+  printf("\t\t\tSEJA BEM VINDO AO JOGO");
+  puts("\t\nDIGITE A OPCAO QUE VC DESEJA JOGAR:\t\n");
+    puts("\t\t\t[01] -> SOMA ");
+    puts("\t\t\t[02] -> MULTIPLICACAO");
+    puts("\t\t\t[03] -> DIVISAO");
+    puts("\t\t\t[04] -> SUBTRACAO");
+    puts("\t\t\t[0] PARA SAIR");
+
+    
+
+  
+
+
+    return 0;
+}
