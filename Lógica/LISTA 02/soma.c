3ec73be85f71c8521d49bd0d17ea1c9a67b9e71d
Klaywin Ryan <klaywindias@gmail.com>
2025-07-30 12:57:06 -0300

    Create soma.c

---
+++ Lógica/LISTA 02/soma.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main (){
+     
+    int numeros;
+    printf("DIGITE 3 NUMEROS");
+    scanf("%i", &numeros);
+    printf("OS NUMEROS QUE VC DIGITOU SAO: %i %i %i", numeros);
+
+    
+
+
+
+
+    return 0;
+}
