63d431b35dc2dd6f1c896791a00973eb37316a17
Klaywin Ryan <klaywindias@gmail.com>
2025-09-16 12:58:00 -0300

    Create ex0.c

---
+++ PONTEIROS/ex0.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(void){
+
+int num1;
+int num2;
+
+printf("DIGITTE UM NUMERO: ");
+scanf("%d", num1);
+
+printf("DIGITTE UM NUMERO: ");
+scanf("%d", num1);
+
+float soma  = num1 + num2;
+
+printf("A SOMA TOTAL EH: %f", &soma);
+
+
+    return 0;
+}
